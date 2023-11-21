76c65215c25e59c02cb3492d723e3ce2f93109d4
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-11-21 14:51:11 -0300

    Função lerUsuarios

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -9,3 +9,14 @@ function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
     /* Executando o comando SQL */
     mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
+
+function lerUsuarios($conexao){
+    // Comando SQL para fazer a leitura de dados (SELECT)
+    $sql = "SELECT id, nome, email, tipo FROM usuarios ORDER BY nome";
+
+    // Execução do comando e armazenamento do resultado da consulta/query
+    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+    // Retornamos o resultado da query transformando em array associativo
+    return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}
