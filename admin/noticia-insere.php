1a468789a9f58980519f7159a4605b72cb9309aa
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-11-23 15:35:14 -0300

    Inserir img

--- admin/noticia-insere.php
+++ admin/noticia-insere.php
@@ -1,5 +1,23 @@
 <?php 
+require_once "../inc/funcoes-noticias.php";
 require_once "../inc/cabecalho-admin.php";
+
+if( isset($_POST['inserir'])){
+	$titulo = $_POST['titulo'];
+	$texto = $_POST['texto'];
+	$resumo = $_POST['resumo'];
+
+/* Obtendo o id da pessoa que está logada na sessão, e que está cadastrando uma notícia. */
+	$usuarioId = $_SESSION['id'];
+
+	/* Capturando dados do arquivo de imagem/foto */
+	$imagem = $_FILES['imagem'];
+
+echo "<pre>";
+var_dump($imagem);
+echo "</pre>";
+}
+
 ?>
 
 
@@ -9,8 +27,9 @@ require_once "../inc/cabecalho-admin.php";
 		<h2 class="text-center">
 		Inserir nova notícia
 		</h2>
-				
-		<form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir">            
+			
+		<!-- O atributo enctype com o valor indicado permite ao formulário receber arquivos -->
+		<form enctype="multipart/form-data" class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir">            
 
 			<div class="mb-3">
                 <label class="form-label" for="titulo">Título:</label>
